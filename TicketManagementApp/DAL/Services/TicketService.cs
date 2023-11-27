b3648b72578b2eb5522bc73e1b331c4ab682330b
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 12:39:41 +0530

    Add files via upload

---
+++ TicketManagementApp/DAL/Services/TicketService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using TicketManagementApp.DAL.Interfaces;
+using TicketManagementApp.Model;
+
+namespace TicketManagementApp.DAL.Services
+{
+    public class TicketService : Interfaces.ITicketService
+    {
+        private Interfaces.ITicketRepository _repository;
+
+        public TicketService(Interfaces.ITicketRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
