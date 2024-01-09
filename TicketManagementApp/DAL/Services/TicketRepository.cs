3fe068a71a634629ce2da5b239cf64a82510633d
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-09 15:09:51 +0530

    Add files via upload

---
+++ TicketManagementApp/DAL/Services/TicketRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TicketManagementApp.DAL.Interfaces;
+
+namespace TicketManagementApp.DAL.Services
+{
+    public class TicketRepository : Interfaces.ITicketRepository
+    {
+        private TicketDbContext _context;
+
+        public TicketRepository(TicketDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.TicketModel GetById(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
